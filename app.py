5844321745116f3c4acc690238add702584c03c2
Анисимов Александр Сергеевич <a.anisimov@phoenixit.ru>
2020-02-18 16:09:36 +0300

    http flask

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import signal
+from flask import Flask
+from cicd_buzz.buzz import generate
+
+app = Flask(__name__)
+
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
+
+@app.route("/")
+def generate_buzz():
+    page = '<html><body><h1>'
+    page += generate.generate_buzz()
+    page += '</h1></body></html>'
+    return page
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))  # port 5000 is the default
